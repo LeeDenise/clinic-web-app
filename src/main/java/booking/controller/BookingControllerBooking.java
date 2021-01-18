b8b81ed7062ad04cc350940237b45a2a9386fc40
Dohui Lee <dohui.lee@edu.sait.ca>
2021-01-17 23:13:14 -0700

    Update BookingControllerBooking.java

--- src/main/java/booking/controller/BookingControllerBooking.java
+++ src/main/java/booking/controller/BookingControllerBooking.java
@@ -39,8 +39,6 @@ public class BookingControllerBooking {
 	public ModelAndView openUserBookingList(HttpServletRequest request) throws Exception {
 		
 		log.debug("----openUserBookingList.do----");
-//		BookingDto db = new BookingDto();
-//		db.setClientId("asdasd");
 		ModelAndView mv = new ModelAndView("booking/bookinglistuser");
 		
 		HttpSession session = request.getSession();
@@ -48,21 +46,11 @@ public class BookingControllerBooking {
 		List<BookingListDto> list = bookingService.openUserBookingList((String)session.getAttribute("clientId"));
 
 		mv.addObject("list", list);
-//		mv.addObject("th", th);
 		
 		return mv;
 	}
 	
-//	@RequestMapping("/booking/openAdminBookingList.do")
-//	public ModelAndView openAdminBookingList() throws Exception {
-//		ModelAndView mv = new ModelAndView("booking/bookinglistadmin");
-//		List<BookingDto> list = bookingService.openAdminBookingList();
-//		mv.addObject("list", list);
-//		
-//		return mv;
-//	}
-	
-	// AJAX 사용해보기
+	// AJAX 
 	@RequestMapping(value="/booking/newBooking.do", method=RequestMethod.GET)
 	public ModelAndView openNewBooking(HttpServletRequest request) throws Exception {
 		ModelAndView mv = new ModelAndView("booking/bookingnew");
@@ -76,23 +64,11 @@ public class BookingControllerBooking {
 	@RequestMapping(value="/booking/newBooking.do", method=RequestMethod.POST)
 	public ModelAndView newBooking(@RequestParam String empNo) throws Exception {
 		log.debug("--------newBooking----------");
-//		log.debug("empNo: " + empNo);
 		log.debug("empNo: " + empNo);
 		
 		ModelAndView mv = new ModelAndView("jsonView");
-		// 사원번호로 스케줄 로드 (오늘 날짜부터 7일)
-		List<BookingDto> list = bookingService.selectTherapist(Integer.parseInt(empNo));
-//		BookingDto db = new BookingDto();
-//		db.setClientId("asdasd");
-//		db.setEmpNo(123);
-//		db.setBookingNo(100);
-//		
-//		List<BookingDto> list = new ArrayList<BookingDto>();
-//		list.add(db);
-//		list.add(db);
-//		list.add(db);
-		
-//		json 데이터 생성 시 object형의 키를 'data'로 설정 
+		List<BookingDto> list = bookingService.selectTherapist(Integer.parseInt(empNo)); //load the employee id
+
 		mv.addObject("data", list);
 		
 		return mv;
@@ -101,29 +77,15 @@ public class BookingControllerBooking {
 	@RequestMapping(value="/booking/newBookingTime.do", method=RequestMethod.POST)
 	public ModelAndView newBookingTime(@RequestParam String empNo, @RequestParam String selDate) throws Exception {
 		log.debug("--------newBookingTime----------");
-//		log.debug("empNo: " + empNo);
 		log.debug("empNo: " + empNo);
 		log.debug("selDate: " + selDate);
 		
 		ModelAndView mv = new ModelAndView("jsonView");
-		// 사원번호로 스케줄 로드 (오늘 날짜부터 7일)
 		
 		log.debug("test");
 		List<BookingDto> list = bookingService.selectBookingTime(Integer.parseInt(empNo), selDate + " 00:00:00", selDate + " 23:59:59");
 		
 		log.debug("--------newBookingTime123123123----------");
-//		BookingDto db = new BookingDto();
-//		db.setClientId("asdasd");
-//		db.setEmpNo(Integer.parseInt(empNo));
-//		db.setStartTime("09:00");
-//		
-//		List<BookingDto> list = new ArrayList<BookingDto>();
-//		list.add(db);
-//		list.add(db);
-//		list.add(db);
-//		
-//		json 데이터 생성 시 object형의 키를 'data'로 설정 
-		
 		log.debug(list.toString());
 		mv.addObject("data", list);
 		
@@ -138,7 +100,6 @@ public class BookingControllerBooking {
 		log.debug("booking: " + booking.getStartTime());
 		log.debug(booking.getEmpNo());
 		
-		// 현재 날짜 불러와서 문자열로 변경 시켜서 값 넣기
 		Date dateNow = new Date();
 		SimpleDateFormat transFormat = new SimpleDateFormat("yyMMddHHmmssSSS");
 		String stringNow = transFormat.format(dateNow);
@@ -167,7 +128,6 @@ public class BookingControllerBooking {
 		return mv;
 	}
 	
-	// 전체불러오는 것 새로 만들기
 	@RequestMapping(value="/booking/bookingDetail.do", method=RequestMethod.POST)
 	public ModelAndView detailNewBooking(HttpServletRequest request) throws Exception {
 		log.debug("-----therapist----");
