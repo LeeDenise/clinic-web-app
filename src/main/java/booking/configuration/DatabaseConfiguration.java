fb9ceab546c62315cc5a3312909737349cab14d8
Dohui Lee <dohui.lee@edu.sait.ca>
2021-01-17 23:10:31 -0700

    Update DatabaseConfiguration.java

--- src/main/java/booking/configuration/DatabaseConfiguration.java
+++ src/main/java/booking/configuration/DatabaseConfiguration.java
@@ -39,9 +39,6 @@ public class DatabaseConfiguration {
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
-		// classpath : resource 폴더를 의미
-		// /sql/**/ : resource/sql 폴더 아래의 모든 폴더를 의미
-		// /sql-*.xml :파일의 이름이 sql-로 시작하고 .xml로 끝나는 모든 파일을 의미
 		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/sql/sql-*.xml"));
 		sqlSessionFactoryBean.setConfiguration(mybatisConfig());
 		return sqlSessionFactoryBean.getObject();
