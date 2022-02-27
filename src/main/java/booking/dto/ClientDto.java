9343106b8b7ddf3712dec0f6a65d78e37fc7461a
denise <denise-at-779403543636>
2022-02-26 21:15:47 -0700

    Added UserDetails.

--- src/main/java/booking/dto/ClientDto.java
+++ src/main/java/booking/dto/ClientDto.java
@@ -1,13 +1,52 @@
 package booking.dto;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
-public class ClientDto {
+public class ClientDto implements UserDetails {
 	private Long clientId;
 	private String email;
 	private String pwd;
 	private String firstName;
 	private String lastName;
 	private String phone;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
