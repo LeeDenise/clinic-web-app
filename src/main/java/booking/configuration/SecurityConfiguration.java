2866793d776792b3820ac15df19e7c4b65eedf89
Nova Kim <novakim.dev@gmail.com>
2022-03-15 11:50:21 -0600

    Fixed profile img loading problem in home.html

--- src/main/java/booking/configuration/SecurityConfiguration.java
+++ src/main/java/booking/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -81,4 +82,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     MappingJackson2JsonView jsonView(){
         return new MappingJackson2JsonView();
     }
+
+    //solving img loading problem
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/js/**");
+        web.ignoring().antMatchers("/image/**");
+    }
 }
+
