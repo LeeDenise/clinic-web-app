cff5f5574af3d666a04e9299db4ba75ccf6c2b2a
Nova Kim <novakim.dev@gmail.com>
2022-03-15 23:17:26 -0600

    Fixed application not running problem imported @RequestParam in UserController.java

--- src/main/java/booking/controller/UserController.java
+++ src/main/java/booking/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
